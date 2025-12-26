353ec1406c58e5962974baaee595812454bfd6b4
Abishek <abishekbalan86@gmail.com>
2025-12-26 21:42:23 +0530

    Add files via upload

---
+++ sketch_dec26a.ino
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+#include <util/delay.h>
+int main(void)
+{
+  DDRB|=(1<<PB1);
+  while(1){
+    PORTB |=(1<<PB1);
+    _delay_ms(5000);
+    PORTB &= ~(1<<PB1);
+    _delay_ms(5000);
+  }
+
+}
